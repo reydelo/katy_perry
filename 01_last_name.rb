044896be447463792a54d51579e2a1c09401e8bf
Reyna DeLoge <reyna.deloge@gmail.com>
2015-02-09 09:23:24 -0700

    test

--- 01_last_name.rb
+++ 01_last_name.rb
@@ -1,3 +1,4 @@
 require_relative "person"
 
 # Print Katy's last name
+puts KATY_PERRY[:last_name]
--- 03_greeting.rb
+++ 03_greeting.rb
@@ -1,3 +1,6 @@
 require_relative "person"
 
 # Print the string "Hello, nice to meet you Katy Perry."
+person = KATY_PERRY["#{:first_name} #{:last_name}"]
+
+puts person
